4b0ebdff4bc6fdc2eb1767af8305e0705d1a63ed
carlos-perez-1166 <arthorcrack166@gmail.com>
2026-03-03 19:24:04 -0600

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'controllers/**/*.js',
+    'middleware/**/*.js',
+    'models/**/*.js',
+    'routes/**/*.js',
+    '!**/node_modules/**',
+    '!**/tests/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true,
+  testTimeout: 10000
+};
